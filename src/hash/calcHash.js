21e3808aaf1ef6c2ea2f05885266d3e8f781b429
Oscarraizer <boltenkovs36@gmail.com>
2025-04-28 23:36:26 +0700

    feat: add hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { createReadStream } from "node:fs";
+import { createHash } from "node:crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = "./src/hash/files/fileToCalculateHashFor.txt";
+  const hash = createHash("sha256");
+  const file = createReadStream(filePath);
+
+  file.on("data", function (chunk) {
+    hash.update(chunk);
+  });
+
+  file.on("end", function () {
+    console.log(hash.digest("hex"));
+  });
 };
 
 await calculateHash();
